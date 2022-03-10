78332611e1999606b8e7090b41fbc45e3a6d19f6
Adrian Lazar <lazar.adrian@gmail.com>
2022-03-10 10:51:27 +0200

    removed _str_

--- application/core/models.py
+++ application/core/models.py
@@ -7,16 +7,12 @@ class Country(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=3)
 
-    def __str__(self):
-        return self.name
 
 
 class County(models.Model):
     name = models.CharField(max_length=100)
     country = models.ForeignKey(Country, related_name='counties', on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
 
 
 class City(models.Model):
