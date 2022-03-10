3b7efe3a360d9cbfc475e4a44231b288b8b444a5
Adrian Lazar <lazar.adrian@gmail.com>
2022-03-10 10:49:45 +0200

    re

--- application/core/views.py
+++ application/core/views.py
@@ -7,15 +7,12 @@ from core.serializers import CountrySerializer, CountySerializer, CitySerializer
 
 
 class CountryViewSet(viewsets.ModelViewSet):
-    """another test descriptipon"""
-    """Test description"""
     serializer_class = CountrySerializer
     queryset = Country.objects.all()
     filterset_class = CountryFilter
 
 
 class CountyViewSet(viewsets.ModelViewSet):
-    "some more description"
     serializer_class = CountySerializer
     queryset = County.objects.all()
     filterset_class = CountyFilter
